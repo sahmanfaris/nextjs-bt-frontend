0941d4e1ba80d9cc13bc402a4792a38c616d1a11
@farissahman1 <farissahman1@gmail.com>
2021-10-22 23:47:28 +0200

    get slug url

--- pages/events/[slug].js
+++ pages/events/[slug].js
@@ -1,12 +1,50 @@
-import { useRouter } from "next/dist/client/router";
 import Layout from "@/components/Layout";
+import { NEXT_URL } from "@/config/index";
 
-const EventPage = () => {
+const EventPage = ({ event }) => {
   return (
     <Layout>
-      <h1>My Event</h1>
+      <h1>{event.name}</h1>
     </Layout>
   );
 };
 
 export default EventPage;
+
+//if we want a request to be made in build time we use combination of get static paths and get static props
+
+export async function getStaticPaths() {
+  const res = await fetch(`${NEXT_URL}/api/events`);
+  const events = await res.json();
+
+  const paths = events.map((e) => ({
+    params: { slug: e.slug },
+  }));
+  return {
+    paths,
+    fallback: true,
+  };
+}
+
+export async function getStaticProps({ params: { slug } }) {
+  const response = await fetch(`${NEXT_URL}/api/events/${slug}`);
+  const events = await response.json();
+  return {
+    props: {
+      event: events[0],
+      revalidate: 1,
+    },
+  };
+}
+
+// else use this down
+
+// export async function getServerSideProps({ query: { slug } }) {
+//   const response = await fetch(`${NEXT_URL}/api/events/${slug}`);
+//   const events = await response.json();
+//   return {
+//     props: {
+//       event: events[0],
+//     },
+//   };
+// }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,7 @@
-import Link from 'next/link'
-import Layout from '@/components/Layout'
-import EventItem from '@/components/EventItem'
-import { NEXT_URL } from '@/config/index'
+import Link from "next/link";
+import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
+import { NEXT_URL } from "@/config/index";
 
 const HomePage = ({ events }) => {
   return (
@@ -9,27 +9,28 @@ const HomePage = ({ events }) => {
       <h1>Upcoming Events</h1>
       {events.length === 0 && <h3>No events to show</h3>}
 
-      {events.map(evt => (
+      {events.map((evt) => (
         <EventItem key={evt.id} evt={evt} />
       ))}
 
       {events.length > 0 && (
-        <Link href='/events'>
-          <a className='btn-secondary'>View All Events</a>
+        <Link href="/events">
+          <a className="btn-secondary">View All Events</a>
         </Link>
       )}
     </Layout>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
 
 export async function getStaticProps() {
-  const res = await fetch(`${NEXT_URL}/api/events`)
-  const events = await res.json()
+  const res = await fetch(`${NEXT_URL}/api/events`);
+  const events = await res.json();
 
   return {
     props: { events: events.slice(0, 3) },
+    // make request again after 1 sec if data is changed
     revalidate: 1,
-  }
+  };
 }
