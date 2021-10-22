dba5003b295b260324d95c35bce68379ca08ee5e
@farissahman1 <farissahman1@gmail.com>
2021-10-22 23:27:17 +0200

    fixed error with events route

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,7 +1,6 @@
-import Layout from '@/components/Layout'
-import EventItem from '@/components/EventItem'
-// import Pagination from '@/components/Pagination'
-// import { API_URL, PER_PAGE } from '@/config/index'
+import Layout from "@/components/Layout";
+import EventItem from "@/components/EventItem";
+import { NEXT_URL } from "@/config/index";
 
 const EventsPage = ({ events }) => {
   return (
@@ -9,32 +8,21 @@ const EventsPage = ({ events }) => {
       <h1>Events</h1>
       {events.length === 0 && <h3>No events to show</h3>}
 
-      {events.map(evt => (
+      {events.map((evt) => (
         <EventItem key={evt.id} evt={evt} />
       ))}
-
-      {/* <Pagination page={page} total={total} /> */}
     </Layout>
-  )
+  );
+};
+
+export default EventsPage;
+
+export async function getStaticProps() {
+  const res = await fetch(`${NEXT_URL}/api/events`);
+  const events = await res.json();
+
+  return {
+    props: { events },
+    revalidate: 1,
+  };
 }
-
-export default EventsPage
-
-// export async function getServerSideProps({ query: { page = 1 } }) {
-//   // Calculate start page
-//   const start = +page === 1 ? 0 : (+page - 1) * PER_PAGE
-
-//   // Fetch total/count
-//   const totalRes = await fetch(`${API_URL}/events/count`)
-//   const total = await totalRes.json()
-
-//   // Fetch events
-//   const eventRes = await fetch(
-//     `${API_URL}/events?_sort=date:ASC&_limit=${PER_PAGE}&_start=${start}`
-//   )
-//   const events = await eventRes.json()
-
-//   return {
-//     props: { events, page: +page, total },
-//   }
-// }
