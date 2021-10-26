f59dae828e2e24ef1c9a1dfdbbcfd81002672c0e
sahmanfaris <farissahman1@gmail.com>
2021-10-26 10:58:47 +0200

    created context

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,9 +1,12 @@
 import Link from 'next/link'
-import styles from '@/styles/Header.module.css'
-import Search from './Search'
+import { useContext } from 'react'
 import { FaSignInAlt, FaSignOutAlt } from 'react-icons/fa'
+import Search from './Search'
+import styles from '@/styles/Header.module.css'
+import AuthContext from '@/context/AuthContext'
 
 const Header = () => {
+  const { user, logout } = useContext(AuthContext)
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
@@ -19,18 +22,40 @@ const Header = () => {
               <a>Events</a>
             </Link>
           </li>
-          <li>
-            <Link href='/events/add'>
-              <a>Add Event</a>
-            </Link>
-          </li>
-          <li>
-            <Link href='/account/login'>
-              <a className='btn-secondary btn-icon'>
-                <FaSignInAlt /> Login
-              </a>
-            </Link>
-          </li>
+          {user ? (
+            <>
+              {' '}
+              <li>
+                <Link href='/events/add'>
+                  <a>Add Event</a>
+                </Link>
+              </li>
+              <li>
+                <Link href='/account/dashboard'>
+                  <a>Dashboard</a>
+                </Link>
+              </li>
+              <li>
+                <button
+                  onClick={() => logout()}
+                  className='btn-secondary btn-icon'
+                >
+                  <FaSignOutAlt /> Logout
+                </button>
+              </li>
+            </>
+          ) : (
+            <>
+              <li>
+                <Link href='/account/login'>
+                  <a className='btn-secondary btn-icon'>
+                    <FaSignInAlt /> Login
+                  </a>
+                </Link>
+              </li>
+            </>
+          )}
+
           {/* <li>
             <Link href='/account/login'>
               <a className='btn-secondary btn-icon'>
--- pages/account/login.js
+++ pages/account/login.js
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import { useContext, useEffect, useState } from 'react'
-// import AuthContext from '@/context/AuthContext'
+import AuthContext from '@/context/AuthContext'
 import { ToastContainer, toast } from 'react-toastify'
 import { FaUser } from 'react-icons/fa'
 import Layout from '@/components/Layout'
@@ -11,13 +11,13 @@ const LoginPage = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
 
-  //   const { login, error } = useContext(AuthContext)
+  const { login, error } = useContext(AuthContext)
 
-  //   useEffect(() => error && toast.error(error))
+  useEffect(() => error && toast.error(error))
 
   const handleSubmit = e => {
     e.preventDefault()
-    // login({ email, password })
+    login({ email, password })
   }
   return (
     <Layout title='User Login'>
