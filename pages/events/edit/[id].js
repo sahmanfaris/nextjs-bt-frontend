6394a81683d0037e8ea1b5e8d8c128e156817a2e
sahmanfaris <farissahman1@gmail.com>
2021-10-26 13:15:17 +0200

    login and register finished with JWT on server

--- pages/events/edit/[id].js
+++ pages/events/edit/[id].js
@@ -1,16 +1,16 @@
-import { useState } from "react";
-import { useRouter } from "next/router";
-import Link from "next/link";
-import Image from "next/image";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
-import { API_URL } from "@/config/index";
-import styles from "@/styles/Form.module.css";
-import Layout from "@/components/Layout";
-import moment from "moment";
-import { FaImage } from "react-icons/fa";
-import Modal from "@/components/Modal";
-import ImageUpload from "@/components/ImageUpload";
+import { useState } from 'react'
+import { useRouter } from 'next/router'
+import Link from 'next/link'
+import Image from 'next/image'
+import { ToastContainer, toast } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { API_URL } from '@/config/index'
+import styles from '@/styles/Form.module.css'
+import Layout from '@/components/Layout'
+import moment from 'moment'
+import { FaImage } from 'react-icons/fa'
+import Modal from '@/components/Modal'
+import ImageUpload from '@/components/ImageUpload'
 
 const EditEventPage = ({ event }) => {
   const [values, setValues] = useState({
@@ -21,123 +21,121 @@ const EditEventPage = ({ event }) => {
     date: event.date,
     time: event.time,
     description: event.description,
-  });
-  const [showModal, setShowModal] = useState(false);
+  })
+  const [showModal, setShowModal] = useState(false)
 
   const [imagePreview, setImagePreview] = useState(
     event.image ? event.image.formats.thumbnail.url : null
-  );
+  )
 
-  const router = useRouter();
+  const router = useRouter()
 
-  const imageUploaded = async (e) => {
-    const res = await fetch(`${API_URL}/events/${event.id}`);
-    const data = await res.json();
-    setImagePreview(data.image.formats.thumbnail.url);
-    setShowModal(false);
-  };
+  const imageUploaded = async e => {
+    const res = await fetch(`${API_URL}/events/${event.id}`)
+    const data = await res.json()
+    setImagePreview(data.image.formats.thumbnail.url)
+    setShowModal(false)
+  }
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
+  const handleSubmit = async e => {
+    e.preventDefault()
 
     // Validation
-    const hasEmptyFields = Object.values(values).some(
-      (element) => element === ""
-    );
+    const hasEmptyFields = Object.values(values).some(element => element === '')
 
     if (hasEmptyFields) {
-      toast.error("Please fill in all fields");
+      toast.error('Please fill in all fields')
     }
 
     const res = await fetch(`${API_URL}/events/${event.id}`, {
-      method: "PUT",
+      method: 'PUT',
       headers: {
-        "Content-Type": "application/json",
+        'Content-Type': 'application/json',
         // Authorization: `Bearer ${token}`,
       },
       body: JSON.stringify(values),
-    });
+    })
 
     if (!res.ok) {
       // if (res.status === 403 || res.status === 401) {
       //   toast.error("No token included");
       //   return;
       // }
-      toast.error("Something Went Wrong");
+      toast.error('Something Went Wrong')
     } else {
-      const evt = await res.json();
-      router.push(`/events/${evt.slug}`);
+      const evt = await res.json()
+      router.push(`/events/${evt.slug}`)
     }
-  };
+  }
 
-  const handleInputChange = (e) => {
-    const { name, value } = e.target;
-    setValues({ ...values, [name]: value });
-  };
+  const handleInputChange = e => {
+    const { name, value } = e.target
+    setValues({ ...values, [name]: value })
+  }
 
   return (
-    <Layout title="Add New Event">
-      <Link href="/events">Go Back</Link>
+    <Layout title='Add New Event'>
+      <Link href='/events'>Go Back</Link>
       <h1>Edit Event</h1>
       <ToastContainer />
       <form onSubmit={handleSubmit} className={styles.form}>
         <div className={styles.grid}>
           <div>
-            <label htmlFor="name">Event Name</label>
+            <label htmlFor='name'>Event Name</label>
             <input
-              type="text"
-              id="name"
-              name="name"
+              type='text'
+              id='name'
+              name='name'
               value={values.name}
               onChange={handleInputChange}
             />
           </div>
           <div>
-            <label htmlFor="performers">Performers</label>
+            <label htmlFor='performers'>Performers</label>
             <input
-              type="text"
-              name="performers"
-              id="performers"
+              type='text'
+              name='performers'
+              id='performers'
               value={values.performers}
               onChange={handleInputChange}
             />
           </div>
           <div>
-            <label htmlFor="venue">Venue</label>
+            <label htmlFor='venue'>Venue</label>
             <input
-              type="text"
-              name="venue"
-              id="venue"
+              type='text'
+              name='venue'
+              id='venue'
               value={values.venue}
               onChange={handleInputChange}
             />
           </div>
           <div>
-            <label htmlFor="address">Address</label>
+            <label htmlFor='address'>Address</label>
             <input
-              type="text"
-              name="address"
-              id="address"
+              type='text'
+              name='address'
+              id='address'
               value={values.address}
               onChange={handleInputChange}
             />
           </div>
           <div>
-            <label htmlFor="date">Date</label>
+            <label htmlFor='date'>Date</label>
             <input
-              type="date"
-              name="date"
-              id="date"
-              value={moment(values.date).format("yyyy-MM-DD")}
+              type='date'
+              name='date'
+              id='date'
+              value={moment(values.date).format('yyyy-MM-DD')}
               onChange={handleInputChange}
             />
           </div>
           <div>
-            <label htmlFor="time">Time</label>
+            <label htmlFor='time'>Time</label>
             <input
-              type="text"
-              name="time"
-              id="time"
+              type='text'
+              name='time'
+              id='time'
               value={values.time}
               onChange={handleInputChange}
             />
@@ -145,17 +143,17 @@ const EditEventPage = ({ event }) => {
         </div>
 
         <div>
-          <label htmlFor="description">Event Description</label>
+          <label htmlFor='description'>Event Description</label>
           <textarea
-            type="text"
-            name="description"
-            id="description"
+            type='text'
+            name='description'
+            id='description'
             value={values.description}
             onChange={handleInputChange}
           ></textarea>
         </div>
 
-        <input type="submit" value="Update Event" className="btn" />
+        <input type='submit' value='Update Event' className='btn' />
       </form>
 
       <h2>Event Image</h2>
@@ -167,7 +165,7 @@ const EditEventPage = ({ event }) => {
         </div>
       )}
       <div>
-        <button onClick={() => setShowModal(true)} className="btn-secondary">
+        <button onClick={() => setShowModal(true)} className='btn-secondary'>
           <FaImage /> Set Image
         </button>
       </div>
@@ -176,16 +174,16 @@ const EditEventPage = ({ event }) => {
         <ImageUpload evtId={event.id} imageUploaded={imageUploaded} />
       </Modal>
     </Layout>
-  );
-};
+  )
+}
 
-export default EditEventPage;
+export default EditEventPage
 
-export async function getServerSideProps({ params: { id } }) {
-  const res = await fetch(`${API_URL}/events/${id}`);
-  const event = await res.json();
+export async function getServerSideProps({ params: { id }, req }) {
+  const res = await fetch(`${API_URL}/events/${id}`)
+  const event = await res.json()
 
   return {
     props: { event },
-  };
+  }
 }
